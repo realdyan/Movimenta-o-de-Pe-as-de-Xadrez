b2be2466afcb5014b4051b866fe2d7706f4cd4f9
Dyan Alencar <dyanalencar@gmail.com>
2025-06-27 17:50:12 +0000

    Adicionado Movimentos do Cavalo

--- xadrez.c
+++ xadrez.c
@@ -6,6 +6,7 @@ int main () {
  int movTorre = 5;
  int movBispo = 5;
  int movRainha = 8;
+ int movCavalo = 1;
  int i;
  
 
@@ -15,6 +16,8 @@ int main () {
     printf("Torre: Direita \n");
  }
  
+  printf("\n");
+
  /* Bispo - Cinco casas na diagonal para cima e à direita.
     Para representar a diagonal, você imprimirá a combinação 
     de duas direções a cada casa (ex: "Cima, Direita"). */
@@ -26,7 +29,8 @@ int main () {
     i++;
  }
 
-    
+ printf("\n");
+
  // Rainha - Oito casas para a esquerda.
  //loop do while
  i = 0;
@@ -34,6 +38,29 @@ int main () {
     printf("Rainha: Esquerda\n");
     i++;
  } while (i < movRainha);
+
+  
+ 
+ 
+ 
+ printf("\n");
+ //Cavalo
+ //O programa deverá imprimir no console a direção de cada
+ //etapa do movimento do Cavalo ("Baixo", "Baixo", "Esquerda")
+
+ for (i = 0; i < movCavalo; i++)
+ {
+   while (i < 2)
+   {
+      printf("Cavalo: Baixo\n");
+      i++;
+   }
+   printf("Cavalo: Esquerda\n");
+ }
+ 
+
+
+
 
 
  return 0;
